b437ac7fbe82ff6fa6a2a8f7d6580844d0002786
Nigel Henshaw <nigel@mobapptuts.com>
2018-01-03 09:40:11 +0900

    download and display using HttpUrlConnection library

--- app/src/main/java/com/mobapptuts/httpimagedownloader/MainActivity.kt
+++ app/src/main/java/com/mobapptuts/httpimagedownloader/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.mobapptuts.httpimagedownloader
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Point
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
 import android.support.constraint.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.Ref
+import org.jetbrains.anko.coroutines.experimental.asReference
+import org.jetbrains.anko.coroutines.experimental.bg
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +31,37 @@ class MainActivity : AppCompatActivity() {
             startChronometer()
             imageView.setImageResource(android.R.color.transparent)
             setImageViewDimensions()
+            downloadBackground()
         }
+    }
+
+    private fun downloadBackground() {
+        val weakRef: Ref<MainActivity> = this.asReference()
+        async(UI) {
+            val data: Deferred<Bitmap?> = bg {
+                loadImageUsingHttpUrlConnection()
+            }
+            val bitmap = data.await()
+            if (bitmap != null) {
+                weakRef().imageView.setImageBitmap(bitmap)
+                weakRef().chronometer.stop()
+            }
+        }
+    }
+
+    private fun loadImageUsingHttpUrlConnection(): Bitmap? {
+        val url = URL(imageUrl)
+        val urlConnection = url.openConnection() as HttpURLConnection
+        var bitmap: Bitmap? = null
+        try {
+            val inputStream = BufferedInputStream(urlConnection.inputStream)
+            bitmap = BitmapFactory.decodeStream(inputStream)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            urlConnection.disconnect()
+        }
+        return bitmap
     }
 
     private fun startChronometer() {
