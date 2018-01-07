944f081701d50b09b7c4423e848cb4e6443a7021
Nigel Henshaw <nigel@mobapptuts.com>
2018-01-07 14:02:53 +0900

    Added resize filtering to HttpUrlConnection

--- app/src/main/java/com/mobapptuts/httpimagedownloader/MainActivity.kt
+++ app/src/main/java/com/mobapptuts/httpimagedownloader/MainActivity.kt
@@ -15,7 +15,9 @@ import org.jetbrains.anko.coroutines.experimental.Ref
 import org.jetbrains.anko.coroutines.experimental.asReference
 import org.jetbrains.anko.coroutines.experimental.bg
 import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
 import java.io.IOException
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -49,13 +51,57 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun convertInputStreamToByteArray(inputStream: InputStream): ByteArray {
+        val byteBuffer = ByteArrayOutputStream()
+        val byteSize = 1024
+        val buffer = ByteArray(byteSize)
+
+        while (true) {
+            val remainingBytes = inputStream.read(buffer)
+            if (remainingBytes < 0) {
+                break
+            }
+            byteBuffer.write(buffer, 0, remainingBytes)
+        }
+        return byteBuffer.toByteArray()
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options): Int {
+        val origWidth = options.outWidth
+        val origHeight = options.outHeight
+        val targetWidth = imageView.layoutParams.width
+        val targetHeight = imageView.layoutParams.height
+        var inSampleSize = 1
+
+        if (origWidth > targetWidth || origHeight > targetHeight) {
+            val halfWidth = origWidth / 2
+            val halfHeight = origHeight / 2
+            while (halfWidth / inSampleSize >= targetWidth && halfHeight / inSampleSize >= targetHeight)
+                inSampleSize *= 2
+        }
+        return inSampleSize
+    }
+
+    private fun decodeSampleBitmapFromByteArray(byteArray: ByteArray): Bitmap {
+        val option = BitmapFactory.Options()
+        option.inJustDecodeBounds = true
+        BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size, option)
+        option.inSampleSize = calculateInSampleSize(option)
+        option.inScaled = true
+        option.inDensity = option.outWidth
+        option.inTargetDensity = imageView.layoutParams.width * option.inSampleSize
+        option.inJustDecodeBounds = false
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size, option)
+    }
+
     private fun loadImageUsingHttpUrlConnection(): Bitmap? {
         val url = URL(imageUrl)
         val urlConnection = url.openConnection() as HttpURLConnection
         var bitmap: Bitmap? = null
         try {
             val inputStream = BufferedInputStream(urlConnection.inputStream)
-            bitmap = BitmapFactory.decodeStream(inputStream)
+            val byteArray = convertInputStreamToByteArray(inputStream)
+            bitmap = decodeSampleBitmapFromByteArray(byteArray)
         } catch (e: IOException) {
             e.printStackTrace()
         } finally {
